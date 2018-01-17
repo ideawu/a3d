071b93933bb15dc0e39ca688aa1b04d083f3435d
ideawu <ideawu@me>
2018-01-17 20:24:43 +0800

    updat

---
+++ a3d/gl/Texture.h
@@ -0,0 +1,10 @@
+//  Created by ideawu on 17/01/2018.
+//  Copyright © 2018 ideawu. All rights reserved.
+//
+
+#ifndef Texture_hpp
+#define Texture_hpp
+
+#include <stdio.h>
+
+#endif /* Texture_hpp */
---
+++ a3d/gl/Texture.m
@@ -0,0 +1,5 @@
+//  Created by ideawu on 17/01/2018.
+//  Copyright © 2018 ideawu. All rights reserved.
+//
+
+#include "Texture.h"
