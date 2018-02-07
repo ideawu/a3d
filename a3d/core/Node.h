fe6af9545caafe2dd7058fd76c9b0263c5ae330e
ideawu <ideawu@me>
2018-02-07 14:16:58 +0800

    update

--- a3d/core/Node.h
+++ a3d/core/Node.h
@@ -35,6 +35,7 @@ namespace a3d{
 		Node* parent() const;
 		void removeFromParent();
 		void addSubnode(Node *node);
+		// index 0 is back
 		void addSubnodeAtIndex(Node *node, int index);
 		void removeSubnode(Node *node);
 		void bringSubnodeToBack(Node *node);
